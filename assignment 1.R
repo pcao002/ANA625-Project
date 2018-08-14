83d44dc11d2376ca3252f1cfb1d5b8b6d1eea594
pcao002 <42254314+pcao002@users.noreply.github.com>
2018-08-13 18:43:38 -0700

    New changes 6:43

--- assignment 1.R
+++ assignment 1.R
@@ -1,2 +1,24 @@
 test = 3 + 6
 # first R code
+
+# mean
+some_num <- rnorm(1000, 3 , 4)
+mean(some_num)
+
+# function for computing Mode 
+Mode = function(x){ 
+  ta = table(x)
+  tam = max(ta)
+  if (all(ta == tam))
+    mod = NA
+  else
+    if(is.numeric(x))
+      mod = as.numeric(names(ta)[ta == tam])
+  else
+    mod = names(ta)[ta == tam]
+  return(mod)}
+
+Mode(some_num)
+median(some_num)
+
+boxplot(some_num, Main = "Box plot of some_num", col = "pink")
